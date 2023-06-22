54a7bd7dd93ca4cf6fa88a68793ff212ab479a44
deekshithepns <70252174+deekshithmd@users.noreply.github.com>
2023-06-22 23:21:59 +0530

    added contract

--- contracts/Ecommerce.sol
+++ contracts/Ecommerce.sol
@@ -5,5 +5,46 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 
 contract Ecommerce {
-    string public name="Ecommerce";
+    address public owner;
+
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    mapping(uint256 => Item) public items;
+
+    constructor(){
+        owner=msg.sender;
+    }
+
+    //List products
+    function list(
+        uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+        ) public {
+        
+        Item memory item = Item(
+            _id,
+            _name,
+            _category,
+            _image,
+            _cost,
+            _rating,
+            _stock);
+        items[_id]=item;
+    }
+    //Buy products
+
+    //Widthdraw funds
 }
--- test/Ecommerce.js
+++ test/Ecommerce.js
@@ -1,22 +1,59 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
 
+// const tokens = (n) => {
+//   console.log(n.toString())
+//   return ethers.utils.parseUnits(n.toString(), "ether");
+// };
 
-const token=(n)=>{
-  return ethers.utils.parseUnits(n.toString(),'ether')
-}
+const ID=1;
+const NAME="Shirt";
+const CATEGORY="Clothing";
+const IMAGE="image";
+const COST=10000000000000000000;
+const RATING=4;
+const STOCK=5;
 
 describe("Ecommerce", function () {
-
   let ecommerceContract;
-  this.beforeEach('Before Each Test',async()=>{
-   const Ecommerce= await ethers.getContractFactory('Ecommerce');
-  ecommerceContract= await Ecommerce.deploy();
-  })
+  let deployer, buyer;
 
- describe('Deploying Contract',()=>{
-  it('Checking Name',async()=>{
-    expect(await ecommerceContract.name()).to.equal('Ecommerce')
-   })
- })
+  this.beforeEach("Before Each Test", async () => {
+    [deployer, buyer] = await ethers.getSigners();
+    const Ecommerce = await ethers.getContractFactory("Ecommerce");
+    ecommerceContract = await Ecommerce.deploy();
+  });
+
+  describe("Deploying Contract", () => {
+    it("Setting Name",async()=>{
+      expect(await ecommerceContract.owner()).to.equal(deployer.address)
+    })
+  });
+
+  describe('Listing',()=>{
+    let transaction;
+
+    beforeEach(async()=>{
+      transaction=await ecommerceContract.connect(deployer).list(
+        ID,
+        NAME,
+        CATEGORY,
+        IMAGE,
+        COST,
+        RATING,
+        STOCK
+      )
+      await transaction.wait();
+    })
+    it("Returns item attributes", async()=>{
+      const item=await ecommerceContract.items(ID)
+      expect(item.id).to.equal(ID)
+      expect(item.name).to.equal(NAME)
+      expect(item.category).to.equal(CATEGORY)
+      expect(item.image).to.equal(IMAGE)
+      expect(item.cost).to.equal(COST)
+      expect(item.rating).to.equal(RATING)
+      expect(item.stock).to.equal(STOCK)
+    })
+  })
 });
